f581890113ace95cc393ef9c015ee3111fdb347e
MazdakShojaie <hrsh@users.noreply.github.com>
2019-09-23 22:09:20 +0330

    Inject HttpClient

--- IZarinpalProvider.cs
+++ IZarinpalProvider.cs
@@ -1,14 +1,24 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Zarinpal.Api.Models;
 
 namespace Zarinpal.Api
 {
     public interface IZarinpalProvider
     {
+        [Obsolete("This method is obsolete. Use PayAsync instead")]
         ZarinpalResult<ZarinpalPaymentResponseModel> InvokePayment(ZarinpalPaymentRequestModel model);
+
+        [Obsolete("This method is obsolete. Use PayAsync instead")]
         Task<ZarinpalResult<ZarinpalPaymentResponseModel>> InvokePaymentAsync(ZarinpalPaymentRequestModel model);
 
+        Task<ZarinpalResult<ZarinpalPaymentResponseModel>> PayAsync(ZarinpalPaymentRequestModel model);
+        Task<ZarinpalResult<ZarinpalVerificationResponseModel>> VerifyAsync(ZarinpalPaymentVerificationModel model);
+
+        [Obsolete("This method is obsolete. Use VerifyAsync instead")]
         ZarinpalResult<ZarinpalVerificationResponseModel> InvokePaymentVerification(ZarinpalPaymentVerificationModel model);
+
+        [Obsolete("This method is obsolete. Use VerifyAsync instead")]
         Task<ZarinpalResult<ZarinpalVerificationResponseModel>> InvokePaymentVerificationAsync(ZarinpalPaymentVerificationModel model);
     }
 }
--- ZarinpalProvider.cs
+++ ZarinpalProvider.cs
@@ -1,8 +1,10 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
@@ -13,11 +15,17 @@ namespace Zarinpal.Api
     public class ZarinpalProvider : IZarinpalProvider
     {
         private readonly ZarinpalConfiguration _configuration;
+        private readonly HttpClient _httpClient;
         private readonly string _requestUrl;
         private readonly string _verifyUrl;
 
-        public ZarinpalProvider(IOptionsSnapshot<ZarinpalConfiguration> options)
+        public ZarinpalProvider(IOptionsSnapshot<ZarinpalConfiguration> options, HttpClient httpClient)
         {
+            _httpClient = httpClient;
+
+            if (_httpClient == null)
+                throw new ArgumentNullException(nameof(_httpClient));
+
             var zarinpalConfiguration = options;
             if (zarinpalConfiguration == null)
                 throw new ArgumentNullException(nameof(zarinpalConfiguration));
@@ -106,6 +114,26 @@ namespace Zarinpal.Api
             }
         }
 
+        private async Task<ZarinpalResult<T>> PostRequestBase<T, TU>(TU model, string url) where T : class
+        {
+            try
+            {
+                var t = await _httpClient.PostAsync(url, 
+                    new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json"));
+                var f = await t.Content.ReadAsAsync<T>();
+                return ZarinpalResult<T>.Invoke(f);
+            }
+            catch
+            {
+                return ZarinpalResult<T>.Failed(new ZarinpalError
+                {
+                    Code = "8000",
+                    Description = "Could not send request!"
+                });
+            }
+        }
+
+        [Obsolete("This method is obsolete. Use Pay instead")]
         public ZarinpalResult<ZarinpalPaymentResponseModel> InvokePayment(ZarinpalPaymentRequestModel model)
         {
             var errors = new List<ZarinpalError>();
@@ -128,6 +156,7 @@ namespace Zarinpal.Api
             return errors.Any() ? ZarinpalResult<ZarinpalPaymentResponseModel>.Failed(errors.ToArray()) : t;
         }
 
+        [Obsolete("This method is obsolete. Use PayAsync instead")]
         public async Task<ZarinpalResult<ZarinpalPaymentResponseModel>> InvokePaymentAsync(ZarinpalPaymentRequestModel model)
         {
             var errors = new List<ZarinpalError>();
@@ -136,6 +165,9 @@ namespace Zarinpal.Api
                 return ZarinpalResult<ZarinpalPaymentResponseModel>.Failed(errors.ToArray());
 
             model.MerchantId = _configuration.Token;
+
+            //var x = await PostRequestBase<ZarinpalPaymentResponseModel, ZarinpalPaymentRequestModel>(model, _requestUrl);
+            
             var request = await StartPaymentAsync(model, ZarinpalRequestMethod.Post, _requestUrl);
             if (!request.Succeeded)
                 return ZarinpalResult<ZarinpalPaymentResponseModel>.Failed(request.Errors.ToArray());
@@ -150,6 +182,26 @@ namespace Zarinpal.Api
             return errors.Any() ? ZarinpalResult<ZarinpalPaymentResponseModel>.Failed(errors.ToArray()) : t;
         }
 
+        public async Task<ZarinpalResult<ZarinpalVerificationResponseModel>> VerifyAsync(ZarinpalPaymentVerificationModel model)
+        {
+            var errors = new List<ZarinpalError>();
+
+            model = new ZarinpalPaymentVerificationModel(_configuration.Token, model.Amount, model.Authority);
+            model.ValidateModel(errors);
+
+            if (errors.Any())
+                return ZarinpalResult<ZarinpalVerificationResponseModel>.Failed(errors.ToArray());
+
+            var t = await PostRequestBase<ZarinpalVerificationResponseModel, ZarinpalPaymentVerificationModel>(
+                model,
+                _verifyUrl);
+
+            return !t.Succeeded ? 
+                ZarinpalResult<ZarinpalVerificationResponseModel>.Failed(errors.ToArray()) : 
+                ZarinpalResult<ZarinpalVerificationResponseModel>.Invoke(t.Result);
+        }
+
+        [Obsolete("This method is obsolete. Use Verify instead")]
         public ZarinpalResult<ZarinpalVerificationResponseModel> InvokePaymentVerification(ZarinpalPaymentVerificationModel model)
         {
             var errors = new List<ZarinpalError>();
@@ -162,15 +214,16 @@ namespace Zarinpal.Api
             var response = StartPayment(model, ZarinpalRequestMethod.Post, _verifyUrl);
 
             if (string.IsNullOrWhiteSpace(response.Result))
-                return ZarinpalResult<ZarinpalVerificationResponseModel>.Failed();
+                return ZarinpalResult<ZarinpalVerificationResponseModel>.Failed(new ZarinpalError { Code = "-6000", Description = "Could not fetch response." });
 
             if (!response.Succeeded)
-                return ZarinpalResult<ZarinpalVerificationResponseModel>.Failed();
+                return ZarinpalResult<ZarinpalVerificationResponseModel>.Failed(new ZarinpalError { Code = "-7000", Description = "could not read response." });
 
             var verificationResult = JsonConvert.DeserializeObject<ZarinpalVerificationResponseModel>(response.Result);
             return ZarinpalResult<ZarinpalVerificationResponseModel>.Invoke(verificationResult);
         }
 
+        [Obsolete("This method is obsolete. Use VerifyAsync instead")]
         public async Task<ZarinpalResult<ZarinpalVerificationResponseModel>> InvokePaymentVerificationAsync(ZarinpalPaymentVerificationModel model)
         {
             var errors = new List<ZarinpalError>();
@@ -191,6 +244,31 @@ namespace Zarinpal.Api
 
             var verificationResult = JsonConvert.DeserializeObject<ZarinpalVerificationResponseModel>(response.Result);
             return ZarinpalResult<ZarinpalVerificationResponseModel>.Invoke(verificationResult);
+        }
+
+        public async Task<ZarinpalResult<ZarinpalPaymentResponseModel>> PayAsync(ZarinpalPaymentRequestModel model)
+        {
+            var errors = new List<ZarinpalError>();
+            model.ValidateModel(errors);
+            if (errors.Any())
+                return ZarinpalResult<ZarinpalPaymentResponseModel>.Failed(errors.ToArray());
+
+            model.MerchantId = _configuration.Token;
+            var t = await PostRequestBase<ZarinpalPaymentResponseModel, ZarinpalPaymentRequestModel>(
+                model,
+                _requestUrl);
+
+            if(!t.Succeeded)
+            {
+                return ZarinpalResult<ZarinpalPaymentResponseModel>.Failed(errors.ToArray());
+            }
+
+            t.Result.PaymentUrl = ZarinpalUrlConfig.GetPaymenGatewayUrl(t.Result.Authority, false);
+            t.Result.Validate(errors);
+
+            return errors.Any()
+                ? ZarinpalResult<ZarinpalPaymentResponseModel>.Failed(errors.ToArray())
+                : ZarinpalResult<ZarinpalPaymentResponseModel>.Invoke(t.Result);
         }
     }
 }
